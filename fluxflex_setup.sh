d1bde4818a499201fea2767f493a46553208c7d4
Akira Yumiyama <yumiyama.akira@gmail.com>
2011-12-25 13:49:02 +0900

    modified fluxflex_setup.sh

--- fluxflex_setup.sh
+++ fluxflex_setup.sh
@@ -2,7 +2,7 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1/
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
-gem install rails --no-ri --no-rdoc
+gem install rails -v 3.1.3 --no-ri --no-rdoc
 gem install mysql --no-ri --no-rdoc
 gem install bundler --no-ri --no-rdoc
 
